44ad19de497863aa9a2d00e2b17288461ba9c6c3
Devansh22M <61380256+Devansh22M@users.noreply.github.com>
2022-11-11 09:20:23 +0530

    Add files via upload

---
+++ binary_exp.h
@@ -0,0 +1,25 @@
+using ll = long long;
+
+ll binpow(ll a, ll b) {
+    ll res = 1;
+    while(b > 0) {
+        if (b & 1) {
+            res = res * a;
+        }
+        b >>= 1;
+        a = a * a;
+    }
+    return res;
+}
+
+ll modpow(ll a, ll b, ll mod) {
+    ll res = 1;
+    while(b > 0) {
+        if (b & 1) {
+            res = res * a % mod;
+        }
+        b >>= 1;
+        a = a * a % mod;
+    }
+    return res;
+}
